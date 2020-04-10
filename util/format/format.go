05075c7a60d69539d977f25b274ec6d2cd254029
周智伟 <august.zhou@guanaitong.com>
2020-04-10 17:50:08 +0800

    增加方法，满足bytes需求。

--- util/format/format.go
+++ util/format/format.go
@@ -5,9 +5,33 @@ import "encoding/json"
 func AsString(v interface{}) string {
 	r, err := json.Marshal(v)
 	if err != nil {
+		return ""
+	}
+	return string(r)
+}
+
+func AsDefaultString(v interface{}) string {
+	r, err := json.Marshal(v)
+	if err != nil {
 		return "{}"
 	}
 	return string(r)
+}
+
+func AsBytes(v interface{}) []byte {
+	r, err := json.Marshal(v)
+	if err != nil {
+		return []byte("")
+	}
+	return r
+}
+
+func AsDefaultBytes(v interface{}) []byte {
+	r, err := json.Marshal(v)
+	if err != nil {
+		return []byte("{}")
+	}
+	return r
 }
 
 func AsJson(d string, v interface{}) error {
