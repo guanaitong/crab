e16fdbfad2028b2bd754e087369516656fbc79a5
shadowpriest@users.noreply.github.com <zhoudevelop@gmail.com>
2020-10-17 20:43:59 +0800

    add work group

--- util/task/task.go
+++ util/task/task.go
@@ -25,28 +25,6 @@ func StartBackgroundTask(name string, period time.Duration, task func()) {
 		}
 	}()
 }
-// when stopped,stoppedSignal will receive signal
-func StartStopAbleBackgroundTask(ctx context.Context, stoppedSignal chan struct{}, name string, period time.Duration, task func()) {
-	go func() {
-		for {
-			select {
-			default:
-				func() {
-					defer runtime.HandleCrashWithConfig(false, func(r interface{}) {
-						callers := runtime.GetCallers(r)
-						msg := fmt.Sprintf("GoroutineName:%s,\nObserved a panic: %#v (%v)\n%v", name, r, r, callers)
-						alert.SendByAppName(4, msg)
-					})
-					task()
-					time.Sleep(period)
-				}()
-			case <-ctx.Done():
-				stoppedSignal <- struct{}{}
-				return
-			}
-		}
-	}()
-}
 
 // 任务只会执行一次
 func StartAsyncTask(name string, task func()) {
