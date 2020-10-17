585f6649ff0cc5aaa9635b3d3b48808673bbb410
shadowpriest@users.noreply.github.com <zhoudevelop@gmail.com>
2020-10-17 20:43:04 +0800

    add work group

--- util/task/task.go
+++ util/task/task.go
@@ -1,9 +1,11 @@
 package task
 
 import (
+	"context"
 	"fmt"
 	"github.com/guanaitong/crab/alert"
 	"github.com/guanaitong/crab/util/runtime"
+	"k8s.io/klog"
 	"time"
 )
 
@@ -36,4 +38,97 @@ func StartAsyncTask(name string, task func()) {
 			task()
 		}()
 	}()
+}
+
+type WorkGroup interface {
+	Start()
+	Stop()
+}
+
+// 因为：context的CancelFunc不会等待任务真正停止
+// 所以有workerGroup的封装：workerGroup的stop方法，会同步等待任务真正stop
+// 一个workerGroup对应一组goroutine，数量为parallelNum
+type workerGroup struct {
+	name         string
+	period       time.Duration
+	taskFunc     func(ctx context.Context)
+	stopSignalCh chan struct{}
+	ctx          context.Context
+	cancel       context.CancelFunc
+	parallelNum  int
+}
+
+func (worker *workerGroup) Start() {
+	klog.Infof("workerGroup [%s] starting", worker.name)
+	for i := 0; i < worker.parallelNum; i++ {
+		worker.startOneGoroutine(i)
+	}
+	klog.Infof("workerGroup [%s] started", worker.name)
+}
+
+const GoroutineIndexKey = "goroutine_index"
+
+func (worker *workerGroup) startOneGoroutine(i int) {
+	go func() {
+		goroutineName := fmt.Sprintf("%s__%d", worker.name, i)
+		for {
+			select {
+			default:
+				func() {
+					defer runtime.HandleCrashWithConfig(false, func(r interface{}) {
+						callers := runtime.GetCallers(r)
+						msg := fmt.Sprintf("GoroutineName:%s,\nObserved a panic: %#v (%v)\n%v", goroutineName, r, r, callers)
+						alert.SendByAppName(4, msg)
+					})
+					// 这个把goroutine的index放到ctx里
+					ctx := context.WithValue(worker.ctx, GoroutineIndexKey, i)
+					worker.taskFunc(ctx)
+					time.Sleep(worker.period)
+				}()
+			case <-worker.ctx.Done():
+				// 收到ctx的停止信号后，给stoppedSignal发一个信号
+				worker.stopSignalCh <- struct{}{}
+				klog.Infof("workerGroup [%s] goroutine_index_[%d] stopped", worker.name, i)
+				return
+			}
+		}
+	}()
+	klog.Infof("workerGroup [%s] goroutine_index_[%d] started", worker.name, i)
+}
+
+// stop方法会等待taskFunc彻底退出
+func (worker *workerGroup) Stop() {
+	klog.Infof("workerGroup [%s] stopping", worker.name)
+	worker.cancel()
+	// 等待真正停止信号
+	stopNum := 0
+	for range worker.stopSignalCh {
+		stopNum++
+		if stopNum == worker.parallelNum {
+			break
+		}
+	}
+	close(worker.stopSignalCh)
+	klog.Infof("workerGroup [%s] stopped", worker.name)
+}
+
+func NewWorkGroup(
+	parentCtx context.Context,
+	name string,
+	period time.Duration,
+	taskFunc func(ctx context.Context),
+	parallelNum int) WorkGroup {
+	if parallelNum < 1 {
+		panic("parallelNum must be positive")
+	}
+	ctx, cancel := context.WithCancel(parentCtx)
+	r := &workerGroup{name: name,
+		period:       period,
+		taskFunc:     taskFunc,
+		stopSignalCh: make(chan struct{}, parallelNum),
+		ctx:          ctx,
+		cancel:       cancel,
+		parallelNum:  parallelNum}
+
+	return r
 }
--- util/task/task_test.go
+++ util/task/task_test.go
@@ -1,9 +1,61 @@
 package task_test
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"github.com/guanaitong/crab/util/task"
+	"testing"
+	"time"
+)
 
 func TestDefault(t *testing.T) {
 	t.Log("pass")
+}
+
+func TestStartRunner1(t *testing.T) {
+	var end = 0
+	r := task.NewWorkGroup(context.Background(), "test", time.Second, func(ctx context.Context) {
+		fmt.Println(ctx.Value(task.GoroutineIndexKey))
+		time.Sleep(5 * time.Second)
+		fmt.Println("end")
+		end++
+	}, 4)
+	r.Start()
+	time.Sleep(time.Second)
+	r.Stop()
+	if end != 4 {
+		t.Fail()
+	}
+}
+
+func TestStartRunner2(t *testing.T) {
+	var end = 0
+	ch := make(chan int, 2)
+	r := task.NewWorkGroup(context.Background(), "test", time.Second, func(ctx context.Context) {
+		fmt.Println(ctx.Value(task.GoroutineIndexKey))
+		for i := range ch {
+			fmt.Printf("num_%d \n", i)
+			time.Sleep(time.Second)
+		}
+		fmt.Println("end")
+		end++
+	}, 2)
+	r.Start()
+	time.Sleep(time.Second)
+	ch <- 3
+	ch <- 3
+	ch <- 3
+	ch <- 3
+	ch <- 2
+	ch <- 2
+	ch <- 2
+	ch <- 1
+	close(ch)
+
+	r.Stop()
+	if end != 2 {
+		t.Fail()
+	}
 }
 
 /*
@@ -23,7 +75,7 @@ func TestStartBackgroundTask(t *testing.T) {
 	time.Sleep(time.Second * 3)
 
 	if count < 3 {
-		t.Errorf("not work")
+		t.Errorf("not workerGroup")
 	}
 }
 
@@ -35,6 +87,6 @@ func TestStartBackgroundTaskCrash(t *testing.T) {
 	})
 	time.Sleep(time.Second * 5)
 	if count < 3 {
-		t.Errorf("not work")
+		t.Errorf("not workerGroup")
 	}
 }*/
